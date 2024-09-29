5b84fe267b7e26efe50e2a73667b085eab87e6dc
Kamaludeen Musa <150486956+maverickkamal@users.noreply.github.com>
2024-09-29 15:25:09 +0100

    Update signin-form.tsx

--- app/signup-form.tsx and scio-ai-planner/app/signin-form.tsx
+++ app/signup-form.tsx and scio-ai-planner/app/signin-form.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const handleGoogleSignup = async () => {
   try {
-    const response = await axios.get('http://localhost:8000/login');
+    const response = await axios.get('https://scio-planning.vercel.app/login');
     if (response.data && response.data.url) {
       window.location.href = response.data.url;
     } else {
